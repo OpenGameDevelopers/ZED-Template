9b399c47f865424a3de6dcdaaa516582e49c77b7
Rico <teh_programerer@interactivelaboratories.com>
2013-10-02 11:44:54 +0100

    |SOURCE| [Common::Game] * The Game class handles the application's lifecycle * Stubbed-out the member functions
    [Linux::Main]
    * Using Game to manage the application's lifecycle

---
+++ Source/Common/Source/Game.cpp
@@ -0,0 +1,41 @@
+#include <Game.hpp>
+#include <cstring>
+#include <System/Memory.hpp>
+
+namespace ZEDTemplate
+{
+	Game::Game( )
+	{
+		m_pWindow = ZED_NULL;
+		m_pRenderer = ZED_NULL;
+		m_pInputManager = ZED_NULL;
+
+		memset( &m_Canvas, 0, sizeof( m_Canvas ) );
+	}
+
+	Game::~Game( )
+	{
+		zedSafeDelete( m_pInputManager );
+		zedSafeDelete( m_pRenderer );
+		zedSafeDelete( m_pWindow );
+	}
+
+	ZED_UINT32 Game::Initialise( )
+	{
+		return ZED_OK;
+	}
+
+	ZED_UINT32 Game::Execute( )
+	{
+		return ZED_OK;
+	}
+
+	void Game::Update( const ZED_UINT64 p_MicroSeconds )
+	{
+	}
+
+	void Game::Render( )
+	{
+	}
+}
+
--- Source/Linux/Source/Main.cpp
+++ Source/Linux/Source/Main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <Game.hpp>
 
 int main( int p_Argc, char **p_ppArgv )
 {
 	std::cout << "ZED Template" << std::endl;
-	return 0;
+
+	ZEDTemplate::Game TheGame;
+
+	if( TheGame.Initialise( ) != ZED_OK )
+	{
+		std::cout << "[ZEDTemplate::main] <ERROR> Failed to initialise the "
+			"game" << std::endl;
+
+		return ZED_FAIL;
+	}
+
+	return TheGame.Execute( );
 }
 
