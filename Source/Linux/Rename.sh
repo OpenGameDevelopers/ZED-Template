1ca9b952dc007acc6ffddc8acb002f3ca49a0790
Rico <teh_programerer@interactivelaboratories.com>
2013-10-11 15:41:23 +0100

    |BUILD CONFIGURATION| [Linux * Fixed issue with the Rename script not inserting the project name

--- Source/Linux/Rename.sh
+++ Source/Linux/Rename.sh
@@ -3,15 +3,13 @@
 # Match all instances of ZEDTEMPLATE and ZEDTemplate, replacing with the user's
 # own project name
 
-UPPERNAME = `echo $1 | tr '[:lower:]' '[:upper:]'`
-
-sed -i "s/ZEDTEMPLATE/$UPPERNAME/g" ./Headers/*.hpp
+sed -i "s/ZEDTEMPLATE/`echo $1 | tr '[:lower:]' '[:upper:]'`/g" ./Headers/*.hpp
 sed -i "s/ZEDTemplate/$1/g" ./Headers/*.hpp
-sed -i "s/ZEDTEMPLATE/$UPPERNAME/g" ./Source/*.cpp
+sed -i "s/ZEDTEMPLATE/`echo $1 | tr '[:lower:]' '[:upper:]'`/g" ./Source/*.cpp
 sed -i "s/ZEDTemplate/$1/g" ./Source/*.cpp
-sed -i "s/ZEDTEMPLATE/$UPPERNAME/g" ./Makefile
+sed -i "s/ZEDTEMPLATE/`echo $1 | tr '[:lower:]' '[:upper:]'`/g" ./Makefile
 sed -i "s/ZEDTemplate/$1/g" ./Makefile
-sed -i "s/ZEDTEMPLATE/$UPPERNAME/g" ../Common/Headers/*.hpp
+sed -i "s/ZEDTEMPLATE/`echo $1 | tr '[:lower:]' '[:upper:]'`/g" ../Common/Headers/*.hpp
 sed -i "s/ZEDTemplate/$1/g" ../Common/Headers/*.hpp
-sed -i "s/ZEDTEMPLATE/$UPPERNAME/g" ../Common/Source/*.cpp
+sed -i "s/ZEDTEMPLATE/`echo $1 | tr '[:lower:]' '[:upper:]'`/g" ../Common/Source/*.cpp
 sed -i "s/ZEDTemplate/$1/g" ../Common/Source/*.cpp
