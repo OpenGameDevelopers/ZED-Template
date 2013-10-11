4e544bd4c8bd610a314d02034ca06e62533de7fa
Rico <teh_programerer@interactivelaboratories.com>
2013-10-11 15:28:28 +0100

    |BUILD CONFIGURATION| [Linux] * Rename bash script will convert the upper-case versions of ZEDTEMPLATE to the   upper-case version of the project's name

--- Source/Linux/Rename.sh
+++ Source/Linux/Rename.sh
@@ -3,13 +3,15 @@
 # Match all instances of ZEDTEMPLATE and ZEDTemplate, replacing with the user's
 # own project name
 
-sed -i "s/ZEDTEMPLATE/$1/g" ./Headers/*.hpp
+UPPERNAME = `echo $1 | tr '[:lower:]' '[:upper:]'`
+
+sed -i "s/ZEDTEMPLATE/$UPPERNAME/g" ./Headers/*.hpp
 sed -i "s/ZEDTemplate/$1/g" ./Headers/*.hpp
-sed -i "s/ZEDTEMPLATE/$1/g" ./Source/*.cpp
+sed -i "s/ZEDTEMPLATE/$UPPERNAME/g" ./Source/*.cpp
 sed -i "s/ZEDTemplate/$1/g" ./Source/*.cpp
-sed -i "s/ZEDTEMPLATE/$1/g" ./Makefile
+sed -i "s/ZEDTEMPLATE/$UPPERNAME/g" ./Makefile
 sed -i "s/ZEDTemplate/$1/g" ./Makefile
-sed -i "s/ZEDTEMPLATE/$1/g" ../Common/Headers/*.hpp
+sed -i "s/ZEDTEMPLATE/$UPPERNAME/g" ../Common/Headers/*.hpp
 sed -i "s/ZEDTemplate/$1/g" ../Common/Headers/*.hpp
-sed -i "s/ZEDTEMPLATE/$1/g" ../Common/Source/*.cpp
+sed -i "s/ZEDTEMPLATE/$UPPERNAME/g" ../Common/Source/*.cpp
 sed -i "s/ZEDTemplate/$1/g" ../Common/Source/*.cpp
