afe2670269afb702ce8a16b5336f5ed94c4283d7
Rico <teh_programerer@interactivelaboratories.com>
2013-10-11 16:38:18 +0100

    |BUILD CONFIGURATION| [Linux] * Rename accounts for spaces in references to "ZED Template"

--- Source/Linux/Rename.sh
+++ Source/Linux/Rename.sh
@@ -5,11 +5,16 @@
 
 sed -i "s/ZEDTEMPLATE/`echo $1 | tr '[:lower:]' '[:upper:]'`/g" ./Headers/*.hpp
 sed -i "s/ZEDTemplate/$1/g" ./Headers/*.hpp
+sed -i "s/ZED\ Template/$1/g" ./Headers/*.hpp
 sed -i "s/ZEDTEMPLATE/`echo $1 | tr '[:lower:]' '[:upper:]'`/g" ./Source/*.cpp
 sed -i "s/ZEDTemplate/$1/g" ./Source/*.cpp
+sed -i "s/ZED\ Template/$1/g" ./Source/*.cpp
 sed -i "s/ZEDTEMPLATE/`echo $1 | tr '[:lower:]' '[:upper:]'`/g" ./Makefile
 sed -i "s/ZEDTemplate/$1/g" ./Makefile
+sed -i "s/ZED\ Template/$1/g" ./Makefile
 sed -i "s/ZEDTEMPLATE/`echo $1 | tr '[:lower:]' '[:upper:]'`/g" ../Common/Headers/*.hpp
 sed -i "s/ZEDTemplate/$1/g" ../Common/Headers/*.hpp
+sed -i "s/ZED\ Template/$1/g" ../Common/Headers/*.hpp
 sed -i "s/ZEDTEMPLATE/`echo $1 | tr '[:lower:]' '[:upper:]'`/g" ../Common/Source/*.cpp
 sed -i "s/ZEDTemplate/$1/g" ../Common/Source/*.cpp
+sed -i "s/ZED\ Template/$1/g" ../Common/Source/*.cpp
